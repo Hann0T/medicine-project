4af096449abe27d6f3515f94dff3d75f51a8ad1b
Hann0T <hansolivas1@gmail.com>
2023-03-15 21:54:15 -0500

    refactor: Favorite Model.

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -13,4 +13,9 @@ class Favorite extends Model
     {
         return $this->belongsTo(Medicine::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
