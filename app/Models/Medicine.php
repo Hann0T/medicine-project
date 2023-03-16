47adc427e3082c0816fa2170a76288e18612d62c
Hann0T <hansolivas1@gmail.com>
2023-03-15 21:19:10 -0500

    feat: Models.

---
+++ app/Models/Medicine.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medicine extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function presentations()
+    {
+        return $this->hasMany(Presentation::class);
+    }
+
+    public function records()
+    {
+        return $this->hasMany(Record::class);
+    }
+}
