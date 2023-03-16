fa2bad412244352ee78c2da034d78afda889018c
Hann0T <hansolivas1@gmail.com>
2023-03-15 21:12:43 -0500

    feat: migrations.

---
+++ database/migrations/2023_03_16_020512_create_medicines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('image_url');
+            $table->text('description');
+            $table->text('composition');
+            $table->string('store_name');
+            $table->text('url');
+            $table->foreignId('category_id')->constrained('categories', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicines');
+    }
+};
---
+++ database/migrations/2023_03_16_020530_create_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('presentation');
+            $table->integer('price');
+            $table->timestamp('date');
+            $table->foreignId('medicine_id')->constrained('medicines', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
